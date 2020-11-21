f49a2559a965b80e02d8437f71b1fd0b90876084
Cerys Gibbins <cerys_gibbins@hotmail.co.uk>
2020-11-21 21:08:38 +0000

    Calling JokeAPI

--- Services/JokeRetriever.php
+++ Services/JokeRetriever.php
@@ -4,7 +4,12 @@ class JokeRetriever
 {
     public function getJoke()
     {
-        return 'Why do gorillas have big nostrils?
-        <br>Because they have big fingers :)';
+        $apiResponse = file_get_contents('https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist&type=twopart');
+
+        $data = json_decode($apiResponse, true);
+
+        return $data['setup'] . '<br>' . $data['delivery'];
+        
+        
     }
 }
