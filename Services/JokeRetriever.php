05488faba5f51174597cefec2d1d811364da1018
Cerys Gibbins <cerys_gibbins@hotmail.co.uk>
2020-11-21 20:45:18 +0000

    Creating joke retriever and getting back joke

---
+++ Services/JokeRetriever.php
@@ -0,0 +1,10 @@
+<?php
+
+class JokeRetriever 
+{
+    public function getJoke()
+    {
+        return 'Why do gorillas have big nostrils?
+        <br>Because they have big fingers :)';
+    }
+}
--- index.php
+++ index.php
@@ -1,8 +1,15 @@
+<?php
+    require_once('Services/JokeRetriever.php');
+
+    $jokeRetriever = new JokeRetriever();
+
+?>
+
 <html>
     <head>
         <title>Random Joke Generator</title>
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?>
+        <?php echo $jokeRetriever->getJoke(); ?>
     </body>
 </html>
