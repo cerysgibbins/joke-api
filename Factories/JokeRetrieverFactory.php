e998e331b19322cc8a91cdc6c7106785f16eac2d
Cerys Gibbins <cerys_gibbins@hotmail.co.uk>
2020-11-21 22:06:51 +0000

    Swapping joke retriever out depending on environment for test purposes

--- Factories/JokeRetrieverFactory.php
+++ Factories/JokeRetrieverFactory.php
@@ -1,11 +1,13 @@
 <?php 
 
 require_once('./Services/JokeAPIRetriever.php');
+require_once('./Services/MockJokeRetriever.php');
 
 class JokeRetrieverFactory 
 {
     public function create()
     {
-        return new JokeAPIRetriever();
+        //return new JokeAPIRetriever();
+        return new MockJokeRetriever();
     }
 }
---
+++ Services/MockJokeRetriever.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('JokeRetriever.php');
+require_once('./Models/Joke.php');
+
+class MockJokeRetriever implements JokeRetriever
+{
+    public function getJoke()
+    {
+        return new Joke(
+            'Why do programmers wear glasses?',
+            'Because they need to C#'
+        );
+    }
+}
